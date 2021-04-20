735949cfca18655ef060f1b1966a917883df634a
Ashes <ashesdangol@hotmail.com>
2021-04-21 02:12:49 +1000

    fixed  .ytp-title, .ytp-pause-overlay {display:none}

--- app/assets/scripts/modules/VideoSlider.js
+++ app/assets/scripts/modules/VideoSlider.js
@@ -2,7 +2,6 @@ class VideoSlider {
   constructor() {
     this.iframeVideo = document.querySelector(".gamer-video__iframe")
     this.gameList = document.querySelectorAll(".gamer-video__navigation li")
-
     this.grabLinks()
   }
 
@@ -13,7 +12,7 @@ class VideoSlider {
   }
 
   transferLink(tt) {
-    this.iframeVideo.setAttribute("src", tt + "?rel=0&amp;autoplay=1;")
+    this.iframeVideo.setAttribute("src", tt + "?rel=0&amp;autoplay=1;showinfo=0?ecver=2;")
   }
 }
 
