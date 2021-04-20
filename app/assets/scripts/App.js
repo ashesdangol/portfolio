d7c83ea2365d0be3894acd558d225004ddf85e8b
Ashes <ashesdangol@hotmail.com>
2021-04-21 00:24:30 +1000

    functional video slider

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,6 +2,7 @@ import "../styles/styles.css"
 import MobileMenu from "./modules/MobileMenu"
 import RevealItems from "./modules/RevealItems"
 import StickyHeader from "./modules/StickyHeader"
+import VideoSlider from "./modules/VideoSlider"
 
 if (module.hot) {
   module.hot.accept()
@@ -11,3 +12,4 @@ let stickyHeader = new StickyHeader()
 let mobileMenu = new MobileMenu()
 new RevealItems(document.querySelectorAll(".about-me .card"), 75)
 /*new RevealItems(document.querySelectorAll(".generic-fonts__header--center"), 0)*/
+new VideoSlider()
---
+++ app/assets/scripts/modules/VideoSlider.js
@@ -0,0 +1,20 @@
+class VideoSlider {
+  constructor() {
+    this.iframeVideo = document.querySelector(".gamer-video__iframe")
+    this.gameList = document.querySelectorAll(".gamer-video__navigation li")
+
+    this.grabLinks()
+  }
+
+  grabLinks() {
+    this.gameList.forEach(el => {
+      el.addEventListener("click", tt => this.transferLink(el.getAttribute("data-src")))
+    })
+  }
+
+  transferLink(tt) {
+    this.iframeVideo.setAttribute("src", tt + "?rel=0&amp;autoplay=1;")
+  }
+}
+
+export default VideoSlider
